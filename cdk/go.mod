db78ca44984778af3de369833084ca04d5629f23
th3matty <matthaeus.malek@aclue.de>
2022-08-14 12:47:53 +0200

    add stack

---
+++ cdk/go.mod
@@ -0,0 +1,11 @@
+module simple_aws_cdk_go_stack
+
+go 1.16
+
+require (
+	github.com/aws/aws-cdk-go/awscdk v1.168.0-devpreview
+	github.com/aws/aws-cdk-go/awscdk/v2 v2.37.1
+	github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2 v2.37.1-alpha.0
+	github.com/aws/constructs-go/constructs/v10 v10.1.65
+	github.com/aws/jsii-runtime-go v1.63.2
+)
---
+++ cdk/newGoLambda.go
@@ -0,0 +1,46 @@
+package main
+
+/*
+	// 	"github.com/aws/aws-cdk-go/awscdk/awslambdago"
+	awslambdago.NewGoFunction(stack, jsii.String("myGoHandler"), &awslambdago.GoFunctionProps{
+		Runtime: awslambda.Runtime_GO_1_X(),
+		Entry:   jsii.String("./basic-api-app/handler-function-get"),
+		Bundling: &awslambdago.BundlingOptions{
+			GoBuildFlags: &[]*string{jsii.String(`-ldflags "-s -w"`)},
+		},
+	})
+
+	// hasherservice
+
+		lambda := awscdklambdagoalpha.NewGoFunction(stack, jsii.String(fmt.Sprintf("hasher-lambda-%s", functionName)), &awscdklambdagoalpha.GoFunctionProps{
+		FunctionName: jsii.String(fmt.Sprintf("hasher-%s", functionName)),
+		Entry:        jsii.String(fmt.Sprintf("../functions/%s/main.go", functionName)),
+		LogRetention: logRetention,
+	})
+*/
+
+import (
+	"fmt"
+	//"os"
+
+	//"github.com/aws/aws-cdk-go/awscdk/awslogs"
+	"github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+func NewGoLambda(stack constructs.Construct, functionName string) awscdklambdagoalpha.GoFunction {
+	// logRetention := awslogs.RetentionDays_FIVE_DAYS
+
+	// if env := os.Getenv("ENVIRONMENT"); env == "live" {
+	// 	logRetention = awslogs.RetentionDays_TWO_WEEKS
+	// }
+
+	lambda := awscdklambdagoalpha.NewGoFunction(stack, jsii.String(fmt.Sprintf("myGoLambda-%s", functionName)), &awscdklambdagoalpha.GoFunctionProps{
+		FunctionName: jsii.String(fmt.Sprintf("myGoLambda-%s", functionName)),
+		Entry:        jsii.String(fmt.Sprintf("../functions/%s/main.go", functionName)),
+		//LogRetention: logRetention,
+	})
+
+	return lambda
+}
